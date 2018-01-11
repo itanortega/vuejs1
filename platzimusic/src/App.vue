096a6cea668d550285a4b48e7a4ae93ff215cd66
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-01-10 19:12:29 -0500

    Estructurando componentes

--- platzimusic/src/App.vue
+++ platzimusic/src/App.vue
@@ -3,17 +3,22 @@
     img(src='./assets/logo.png')
     h1 PlatziMusic
     ul
-      li(v-for="artist in artists") {{ artist.name }}
+      artist(v-for="artist in artists" :artist="artist" :key="artist.mbid")
 </template>
 
 <script>
+import Artist from './components/Artist.vue'
 import getArtists from './api'
+
 export default {
   name: 'app',
   data () {
     return {
       artists: []
     }
+  },
+  components: {
+    Artist
   },
   mounted: function (){
     const self = this
